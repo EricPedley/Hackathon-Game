b854cad865f2b47af1d840c0f89054daae48cb07
skoka <skoka@us.ibm.com>
2019-03-23 15:01:51 -0700

    room

--- src/rishab/Room.java
+++ src/rishab/Room.java
@@ -3,6 +3,7 @@ package rishab;
 import java.util.ArrayList;
 
 import Adele.Projectile;
+import processing.core.PApplet;
 
 public class Room { 
 	
@@ -11,7 +12,7 @@ public class Room {
 	private ArrayList<Tile> tileIndices;
 	private ArrayList<Projectile> projectiles;
 	public static final int TILE_SIZE = 20; 
-	
+	private String fs = System.getProperty("file.separator");
 	
 	public Room() {
 		tileType = new int [x][y];
@@ -21,7 +22,8 @@ public class Room {
 	}
 	
 	public void assignIndices() {
-		tileIndices.add();
+		tileIndices.add(new Tile(new PApplet().loadImage("Image"+fs+"Map"+fs+"Wall"+fs+"Rock1BottomFront.gif"),0));
+		tileIndices.add(new Tile(new PApplet().loadImage("Image"+fs+"Map"+fs+"Floor"+fs+"1Tile.gif"),2));
 	}
 	
 	public void assignTiles() {
@@ -32,14 +34,14 @@ public class Room {
 					tileType[rows][cols] = 0;
 				}
 				else {
-					tileType[rows][cols] = 1;
+					tileType[rows][cols] = 2;
 				}
 			}
 		}
 	}
 
 	public Tile tileAt(int x, int y) {
-		
+		return tileIndices.get(tileType[x][y]);
 	}
 	
 	public void deleteProjectile(Projectile p) {
