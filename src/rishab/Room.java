6df73840d03d24d60f65228e350fb3c6b9c40da6
skoka <skoka@us.ibm.com>
2019-03-23 14:48:00 -0700

    hi

--- src/rishab/Room.java
+++ src/rishab/Room.java
@@ -1,15 +1,27 @@
 package rishab;
 
+import java.util.ArrayList;
+
+import Adele.Projectile;
+
 public class Room { 
 	
 	private int[][] tileType;
 	private int x = 10 , y =10;
+	private ArrayList<Tile> tileIndices;
+	private ArrayList<Projectile> projectiles;
+	public static final int TILE_SIZE = 20; 
+	
 	
 	public Room() {
 		tileType = new int [x][y];
 	}
 	public Room(int rows, int cols) {
 		
+	}
+	
+	public void assignIndices() {
+		tileIndices.add();
 	}
 	
 	public void assignTiles() {
@@ -26,4 +38,12 @@ public class Room {
 		}
 	}
 
+	public Tile tileAt(int x, int y) {
+		
+	}
+	
+	public void deleteProjectile(Projectile p) {
+		
+	}
+	
 }
--- src/rishab/Tile.java
+++ src/rishab/Tile.java
@@ -1,5 +1,19 @@
 package rishab;
 
+import processing.core.PImage;
+
 public class Tile {
+	
+	private PImage image;
+	private int type;
+	
+	public Tile(PImage image, int type ) { 
+		this.type = type;
+		this.image = image;
+	}
+	
+	public int getType() {
+		return type;
+	}
 
 }
