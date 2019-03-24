124055e94905536e3a2b849b7ba70fb74e46e4a4
skoka <skoka@us.ibm.com>
2019-03-24 03:21:08 -0700

    yes

--- src/Logan/ImageLoader.java
+++ src/Logan/ImageLoader.java
@@ -6,9 +6,14 @@ import processing.core.PImage;
 public class ImageLoader {
 	private static String fs = System.getProperty("file.separator");
 	public static PImage RED_PROJECTILE, GREEN_PROJECTILE, YELLOW_STAR, PURPLE_STAR, MAIN_CHARACTER_LEFT,
-			MAIN_CHARACTER_RIGHT, BOSS_PILLAR, BOSS;
+			MAIN_CHARACTER_RIGHT, BOSS_PILLAR, BOSS,MINION_RIGHT,MINION_LEFT,SLIME_RIGHT,SLIME_LEFT;
 
 	public static void setUp(PApplet p) {
+		
+		SLIME_RIGHT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "SLIME Right.gif");
+		SLIME_LEFT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "SLIME Left.gif");
+		MINION_RIGHT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "Minion Knight Right.gif");
+		MINION_LEFT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "Minion Knight Left.gif");
 		RED_PROJECTILE = p.loadImage("Images" + fs + "Projectiles" + fs + "Red Projectile.gif");
 		GREEN_PROJECTILE = p.loadImage("Images" + fs + "Projectiles" + fs + "Green Projectile.gif");
 		YELLOW_STAR = p.loadImage("Images" + fs + "Projectiles" + fs + "Yellow Star.gif");
--- src/eric/Enemy2.java
+++ src/eric/Enemy2.java
@@ -5,22 +5,22 @@ import processing.core.PApplet;
 import processing.core.PImage;
 import rishab.Room;
 
-public class Enemy extends Character {
+public class Enemy2 extends Character {
 
 	/*
 	 * public Enemy(double x, double y, PImage image, Room env) { super(x, y,
 	 * image.width,image.height,image,env); // TODO Auto-generated constructor stub
 	 * }
 	 */
-	public Enemy(double x, double y, Room env) {
+	public Enemy2(double x, double y, Room env) {
 		super(x, y, 23*3,30*3,null, env);
 	}
 	
 	public void draw(PApplet marker) {
 		if(super.getDirection()==0) {
-			super.img=ImageLoader.MINION_LEFT;
+			super.img=ImageLoader.SLIME_LEFT;
 		} else {
-			super.img=ImageLoader.MINION_RIGHT;
+			super.img=ImageLoader.SLIME_RIGHT;
 		}
 		img.resize((int)hitbox.width, (int)hitbox.height);
 		super.draw(marker);
