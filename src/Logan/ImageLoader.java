4a062afbb0ef0aabda70fbc6a961fca21956656d
ericpedley <ericpedley@10.232.37.91>
2019-03-24 07:51:47 -0700

    changed projectile color

--- src/Logan/ImageLoader.java
+++ src/Logan/ImageLoader.java
@@ -7,7 +7,7 @@ public class ImageLoader {
 	private static String fs = System.getProperty("file.separator");
 	public static PImage[] EXPLOSION = new PImage[13];
 	public static PImage RED_PROJECTILE, GREEN_PROJECTILE, YELLOW_STAR, PURPLE_STAR, MAIN_CHARACTER_LEFT,
-			MAIN_CHARACTER_RIGHT, BOSS_PILLAR, BOSS,MINION_RIGHT,MINION_LEFT,SLIME_RIGHT,SLIME_LEFT,GAME_OVER;
+			MAIN_CHARACTER_RIGHT, BOSS_PILLAR, BOSS,MINION_RIGHT,MINION_LEFT,SLIME_RIGHT,SLIME_LEFT,GAME_OVER,BLUE_PROJECTILE;
 
 	public static void setUp(PApplet p) {
 		
@@ -16,6 +16,7 @@ public class ImageLoader {
 		MINION_RIGHT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "Minion Knight Right.gif");
 		MINION_LEFT =p.loadImage("Images" + fs + "Characters" + fs + "Enemy" + fs + "Minion Knight Left.gif");
 		RED_PROJECTILE = p.loadImage("Images" + fs + "Projectiles" + fs + "Red Projectile.gif");
+		BLUE_PROJECTILE = p.loadImage("Images" + fs + "Projectiles" + fs + "Blue Projectile.gif");
 		GREEN_PROJECTILE = p.loadImage("Images" + fs + "Projectiles" + fs + "Green Projectile.gif");
 		YELLOW_STAR = p.loadImage("Images" + fs + "Projectiles" + fs + "Yellow Star.gif");
 		PURPLE_STAR = p.loadImage("Images" + fs + "Projectiles" + fs + "Purple Star.gif");
