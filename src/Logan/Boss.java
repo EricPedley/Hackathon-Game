3ccdb7ef4bea6554d45900d4b06638773066bb7b
PagelerLogan <PagelerLogan@10.232.43.4>
2019-03-23 19:17:10 -0700

    t

---
+++ src/Logan/Boss.java
@@ -0,0 +1,14 @@
+package Logan;
+
+public class Boss {
+	private int health = 1000;
+	private int frame = 0;
+	private int x;
+	private int y;
+	
+	public Boss(int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+	}
+}
