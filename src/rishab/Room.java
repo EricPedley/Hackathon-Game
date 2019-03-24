ade46d0036596544eec974842d0e2e8897695a15
skoka <skoka@us.ibm.com>
2019-03-23 17:46:25 -0700

    j

--- src/rishab/Room.java
+++ src/rishab/Room.java
@@ -1,6 +1,10 @@
 package rishab;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import Adele.Projectile;
 import processing.core.PApplet;
@@ -9,7 +13,7 @@ public class Room {
 	
 	private int[][] tileType;
 	private int x = 60 , y = 26;
-	private Tile[] tileIndices = new Tile[2];
+	private Tile[] tileIndices = new Tile[6];
 	private ArrayList<Projectile> projectiles;
 	public static final int TILE_SIZE = 32; 
 	private String fs = System.getProperty("file.separator");
@@ -17,7 +21,8 @@ public class Room {
 	public Room(PApplet p) {
 		tileType = new int [x][y];
 		assignIndices(p);
-		assignTiles();
+		readData("Levels"+fs+"Level1",tileType);
+		//assignTiles();
 		
 	}
 	public Room(int rows, int cols, PApplet p) {
@@ -25,24 +30,74 @@ public class Room {
 	}
 	
 	public void assignIndices(PApplet p) {
-		tileIndices[0] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"RockBottomFront.gif"),0);
-		tileIndices[1] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Floor"+fs+"1Tile.gif"),2);
+		tileIndices[0] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"LeftRockWall.gif"),0);
+		tileIndices[1] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"RightRockWall.gif"),1);
+		tileIndices[2] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"RockTopFront.gif"),2);
+		tileIndices[3] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"RockBottomFront.gif"),3);
+		tileIndices[4] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Wall"+fs+"RockWallBack.gif"),4);
+		tileIndices[5] = new Tile(p.loadImage("Images"+fs+"Map"+fs+"Floor"+fs+"1Tile.gif"),5);
 	}
 	
-	public void assignTiles() {
+	/*public void assignTiles() {
 		
 		for(int rows = 0; rows < x;rows++) {
 			for(int cols = 0; cols < y; cols++) {
-				if(rows == 0 || rows == x-1 || cols == 0 || cols == y-1 || cols == 1) {
+				if(rows == 0) {
 					tileType[rows][cols] = 0;
 				}
-				else {
+				else if(rows == y-1) {
 					tileType[rows][cols] = 1;
+				}
+				else if(cols == 0) {
+					tileType[rows][cols] = 2;
+				}
+				else if(cols == 1) {
+					tileType[rows][cols] = 3;
+				}
+				else if(cols == x-1) {
+					tileType[rows][cols] = 4;
+				}
+				else {
+					tileType[rows][cols] = 5;
 				}
 			}
 		}
-	}
+	}*/
 
+	
+	public void readData (String filename, int[][] gameData) {
+		File dataFile = new File(filename);
+
+		if (dataFile.exists()) {
+			int count = 0;
+
+			FileReader reader = null;
+			Scanner in = null;
+			try {
+					reader = new FileReader(dataFile);
+					in = new Scanner(reader);
+
+					while (in.hasNext()) {
+						String line = in.nextLine();
+						for(int i = 0; i < line.length(); i++)
+							if (i < gameData.length && count < gameData[i].length)
+								gameData[i][count] = line.charAt(i);
+
+						count++;
+					}
+			} catch (IOException ex) {
+				throw new IllegalArgumentException("Data file " + filename + " cannot be read.");
+			} finally {
+				if (in != null)
+					in.close();
+			}
+			
+		} else {
+			throw new IllegalArgumentException("Data file " + filename + " does not exist.");
+		}
+	}
+	
+	
 	public Tile tileAt(int x, int y) {
 		return tileIndices[tileType[x][y]];
 	}
@@ -54,10 +109,10 @@ public class Room {
 	public void draw(PApplet p) {
 		
 		p.pushMatrix();
-		for(int rows = 0; rows < y;rows++) {
+		for(int cols = 0; cols < y;cols++) {
 			
-			for(int cols = 0; cols < x; cols++) {
-				tileIndices[tileType[cols][rows]].draw(p, TILE_SIZE);
+			for(int rows = 0; rows < x; rows++) {
+				tileIndices[tileType[rows][cols]].draw(p, TILE_SIZE);
 				p.translate(TILE_SIZE, 0);
 			}
 			p.translate(-x * TILE_SIZE, TILE_SIZE);
